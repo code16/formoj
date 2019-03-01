7bab3f5d6e37d8f56c71620abb0bcba6dcdef29b
antoine <antoine.guingand@wanadoo.fr>
2019-03-01 14:02:33 +0100

    Setup select field

--- packages/formoj/src/components/fields/Select.vue
+++ packages/formoj/src/components/fields/Select.vue
@@ -1,9 +1,38 @@
 <template>
+    <div class="fj-select">
+        <template v-if="isMultiple">
 
+        </template>
+        <template v-else>
+            <select class="fj-select__input" v-bind="$attrs" @change="handleChanged">
+                <template v-for="option in options">
+                    <option :value="option.id" :key="option.id">{{ option.label }}</option>
+                </template>
+            </select>
+        </template>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'FjSelect',
+
+        props: {
+            type: String,
+            multiple: Boolean,
+            options: Array,
+        },
+
+        computed: {
+            isMultiple() {
+                return this.multiple;
+            },
+        },
+
+        methods: {
+            handleChanged(e) {
+                this.$emit('input', e.target.value);
+            }
+        },
     }
 </script>
