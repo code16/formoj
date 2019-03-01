6b80ee55c5b076078f25429512121a76044f1b8f
antoine <antoine.guingand@wanadoo.fr>
2019-03-01 12:02:26 +0100

    Update example import package name

--- packages/formoj/example/src/main.js
+++ packages/formoj/example/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueFormoj from 'vue-formoj/src';
+import Formoj from 'formoj/src';
 
 Vue.config.productionTip = false;
 
-Vue.use(VueFormoj, { apiBaseUrl: '/api' });
+Vue.use(Formoj, { apiBaseUrl: '/api' });
 
 new Vue({
     render: h => h(App),
