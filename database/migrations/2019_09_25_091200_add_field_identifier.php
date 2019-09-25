e22020a14de3d3e2c08236557bc315779bfdc252
Arnaud Becher <arnaud.becher@gmail.com>
2019-09-25 12:37:36 +0200

    generating field identifiers for existing fields

--- database/migrations/2019_09_25_091200_add_field_identifier.php
+++ database/migrations/2019_09_25_091200_add_field_identifier.php
@@ -1,8 +1,10 @@
 <?php
 
+use Code16\Formoj\Models\Field;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class AddFieldIdentifier extends Migration
 {
@@ -14,7 +16,39 @@ class AddFieldIdentifier extends Migration
     public function up()
     {
         Schema::table('formoj_fields', function (Blueprint $table) {
-            $table->string("identifier");
+
+            if (config("app.env") != "testing") {
+                //$table->string("identifier");
+                $this->fillEmptyFieldIdentifiers();
+            }else{
+                $table->string("identifier")->default('');
+            }
         });
+    }
+
+    public function fillEmptyFieldIdentifiers()
+    {
+
+        Field::where('identifier','=','')
+            ->get()
+            ->each(function(Field $field){
+
+                $slug = $field->label ? Str::slug($field->label,'_') : 'unamed_field';
+                $generatedIdentifier = $slug;
+                $i = 1;
+
+                while(
+                    Field::join('formoj_sections','formoj_sections.id','=','section_id')
+                        ->where('form_id','=',$field->section->form->id)
+                        ->where('formoj_fields.identifier','=',$generatedIdentifier)
+                        ->exists()
+                ){
+                    $generatedIdentifier = $slug . '_' . $i;
+                    $i++;
+                }
+
+                $field->identifier = $generatedIdentifier;
+                $field->save();
+            });
     }
 }
