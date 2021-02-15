ad40a5086e837793208fc6d073da9ce149dbe766
philippe <philippe@code16.fr>
2021-02-15 14:38:06 +0100

    Add test

---
+++ tests/Feature/FormojAnswerControllerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Code16\Formoj\Tests\Feature;
+
+use Code16\Formoj\Models\Answer;
+use Code16\Formoj\Models\Field;
+use Code16\Formoj\Tests\FormojTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FormojAnswerControllerTest extends FormojTestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function we_cant_get_a_non_existing_answer()
+    {
+        $this->get("/formoj/api/answer/1")
+            ->assertStatus(404);
+    }
+
+    /** @test */
+    function we_can_get_a_answer_with_fields()
+    {
+        $field = factory(Field::class)->create([
+            "label" => "Field label",
+            "identifier" => "field_1",
+            "type" => "select",
+        ]);
+        
+        $answer = factory(Answer::class)->create([
+            'content' => [
+                "field_1" => "some answer"
+            ],
+            'form_id' => $field->section->form_id
+        ]);
+
+        $this->get("/formoj/api/answer/{$answer->id}")
+            ->assertStatus(200)
+            ->assertJson([
+                "data" => [
+                    "content" => [
+                        "field_1" => "some answer"
+                    ],
+                    "fields" => [
+                        [
+                            "key" => "field_1",
+                            "label" => "Field label",
+                            "type" => "select"
+                        ]
+                    ]
+                ]
+            ]);
+    }
+
+    /** @test */
+    function we_get_a_default_if_the_field_is_no_longer_present()
+    {
+        $field = factory(Field::class)->create([
+            "label" => "Field label",
+            "identifier" => "field_1",
+            "type" => "select",
+        ]);
+
+        $answer = factory(Answer::class)->create([
+            'content' => [
+                "field_2" => "some answer" // Another (missing) field
+            ],
+            'form_id' => $field->section->form_id
+        ]);
+
+        $this->get("/formoj/api/answer/{$answer->id}")
+            ->assertStatus(200)
+            ->assertJson([
+                "data" => [
+                    "content" => [
+                        "field_2" => "some answer"
+                    ],
+                    "fields" => [
+                        [
+                            "key" => "field_2",
+                            "label" => "field_2",
+                            "type" => "text"
+                        ]
+                    ]
+                ]
+            ]);
+    }
+}
