a2282c4199558b014db2a7a3443b77fc4f78e392
philippe <philippe@code16.fr>
2021-07-06 14:35:27 +0200

    Fix field query in AnswerResource (scope to current form)

--- src/Models/Resources/AnswerResource.php
+++ src/Models/Resources/AnswerResource.php
@@ -3,6 +3,7 @@
 namespace Code16\Formoj\Models\Resources;
 
 use Code16\Formoj\Models\Field;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AnswerResource extends JsonResource
@@ -10,16 +11,19 @@ class AnswerResource extends JsonResource
     public function toArray($request)
     {
         $formFields = Field::whereIn('identifier', collect($this->content)->keys())
+            ->whereHas("section", function(Builder $query) {
+                return $query->where("form_id", $this->form_id);
+            })
             ->get();
-        
+
         return [
             'id' => $this->id,
             'content' => $this->content,
             'fields' => collect($this->content)
                 ->map(function($value, $identifier) use($formFields) {
                     /** @var Field $field */
-                    $field = $formFields->where('identifier', $identifier)->first();
-                    
+                    $field = $formFields->firstWhere('identifier', $identifier);
+
                     return $field
                         ? [
                             'id' => $field->getFrontId(),
--- tests/Feature/FormojAnswerControllerTest.php
+++ tests/Feature/FormojAnswerControllerTest.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class FormojAnswerControllerTest extends FormojTestCase
 {
     use RefreshDatabase;
-
+    
     /** @test */
     function we_cant_get_a_non_existing_answer()
     {
@@ -21,6 +21,8 @@ class FormojAnswerControllerTest extends FormojTestCase
     /** @test */
     function we_can_get_a_answer_with_fields()
     {
+        $this->withoutExceptionHandling();
+        
         $field = factory(Field::class)->create([
             "label" => "Field label",
             "identifier" => "field_1",
@@ -55,6 +57,8 @@ class FormojAnswerControllerTest extends FormojTestCase
     /** @test */
     function we_allow_missing_fields()
     {
+        $this->withoutExceptionHandling();
+        
         $answer = factory(Answer::class)->create([
             'content' => [
                 "some_field" => "some answer" // a missing field
@@ -73,5 +77,48 @@ class FormojAnswerControllerTest extends FormojTestCase
                 ]
             ])
             ->assertJsonCount(0, "data.fields");
+    }
+
+    /** @test */
+    function we_can_get_only_field_of_the_current_answer()
+    {
+        $this->withoutExceptionHandling();
+
+        // Noise
+        factory(Field::class)->create([
+            "label" => "Field label",
+            "identifier" => "field_1",
+            "type" => "text",
+        ]);
+
+        $field = factory(Field::class)->create([
+            "label" => "Field label",
+            "identifier" => "field_1",
+            "type" => "select",
+        ]);
+
+        $answer = factory(Answer::class)->create([
+            'content' => [
+                "field_1" => "some answer"
+            ],
+            'form_id' => $field->section->form_id
+        ]);
+
+        $this->get("/formoj/api/answer/{$answer->id}")
+            ->assertStatus(200)
+            ->assertJson([
+                "data" => [
+                    "content" => [
+                        "field_1" => "some answer"
+                    ],
+                    "fields" => [
+                        [
+                            "name" => "field_1",
+                            "label" => "Field label",
+                            "type" => "select"
+                        ]
+                    ]
+                ]
+            ]);
     }
 }
