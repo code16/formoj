001b579a6ff2df4371e925fc79a0a2dd3d8ccb45
antoine <antoine.guingand@wanadoo.fr>
2024-01-22 14:45:07 +0100

    wip

--- packages/formoj/src/components/fields/Rating.vue
+++ packages/formoj/src/components/fields/Rating.vue
@@ -1,32 +1,34 @@
 <template>
     <div class="fj-radios fj-rating">
-        <template v-if="lowestLabel">
-            <div class="fj-rating__label fj-rating__label--lowest">
-                {{ lowestLabel }}
-            </div>
-        </template>
-        <div class="fj-rating__list">
-            <template v-for="rating in Array.from({length: 5}, (x, i) => i + 1)">
-                <label class="fj-rating__item">
-                    <div class="fj-radios__check fj-rating__check">
-                        <input type="radio"
-                            class="fj-radios__check-input"
-                            :name="name"
-                            :checked="value === rating"
-                            @change="$emit('input', rating)"
-                        >
-                        <div class="fj-radios__check-label">
-                            {{ rating }}
+        <div class="fj-rating__inner">
+            <template v-if="lowestLabel">
+                <div class="fj-rating__label fj-rating__label--lowest">
+                    {{ lowestLabel }}
+                </div>
+            </template>
+            <div class="fj-rating__list">
+                <template v-for="rating in Array.from({length: 5}, (x, i) => i + 1)">
+                    <label class="fj-rating__item">
+                        <div class="fj-radios__check fj-rating__check">
+                            <input type="radio"
+                                class="fj-radios__check-input"
+                                :name="name"
+                                :checked="value === rating"
+                                @change="$emit('input', rating)"
+                            >
+                            <div class="fj-radios__check-label">
+                                {{ rating }}
+                            </div>
                         </div>
-                    </div>
-                </label>
+                    </label>
+                </template>
+            </div>
+            <template v-if="highestLabel">
+                <div class="fj-rating__label fj-rating__label--highest">
+                    {{ highestLabel }}
+                </div>
             </template>
         </div>
-        <template v-if="highestLabel">
-            <div class="fj-rating__label fj-rating__label--highest">
-                {{ highestLabel }}
-            </div>
-        </template>
     </div>
 </template>
 
